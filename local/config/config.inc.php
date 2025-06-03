5b9005d756f07723bdb4cb0f2c3d525bc45ca453
Merhawi <merhawigebreslassie7@gmail.com>
2025-06-04 00:11:06 +0300

    Add local mail settings configuration to enable admin send invitation link for registered users

---
+++ local/config/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+// Local configuration file for Piwigo mail settings
+
+// SMTP configuration for Gmail
+$conf['smtp_host'] = 'smtp.gmail.com:587';
+$conf['smtp_user'] = 'merhawigebreslassie7@gmail.com';
+$conf['smtp_password'] = 'qhku dzsj cgno iqaw';
+$conf['smtp_secure'] = 'tls';
+
+// Mail sender name and email
+$conf['mail_sender_name'] = 'Lazana';
+$conf['mail_sender_email'] = 'merhawigebreslassie7@gmail.com';
+
+// Other mail settings
+$conf['mail_allow_html'] = true;
+$conf['send_bcc_mail_webmaster'] = false;
+
+?>
